a46cee2a745902ee4d588db0d1b461b0869788c0
amanullahgit <amanullah.askari@gmail.com>
2024-10-13 22:33:05 +0530

    Chapter 6 Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:getx_chapter_1/app/modules/product/controllers/cart_controller.dart';
+import 'package:getx_chapter_1/app/services/cart_service.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  Get.put(CartService());
   runApp(MyApp());
 }
 
